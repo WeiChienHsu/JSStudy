409c13dd523e364abf3586a8eb99174796e7b9e8
zhufuzz <zhufuzz@hotmail.com>
2018-06-21 16:45:38 -0400

    add Udemy Angular6 code

---
+++ Udemy_Angular6/_278_optimizations-feature-shared-module/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeService } from './recipes/recipe.service';
+import { DataStorageService } from './shared/data-storage.service';
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
+import { RecipesModule } from './recipes/recipes.modules';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,
+    RecipesModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
