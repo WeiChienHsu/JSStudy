3ae1a13ee86770a7d8a0d5710db86ac130d9d9f3
b_U16 <zhufuzz@hotmail.com>
2018-07-20 09:39:29 -0400

    cs503/week01/oj-client create dataservice to provide data

--- bittiger_cs503/week01/oj-client/src/app/app.module.ts
+++ bittiger_cs503/week01/oj-client/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 
+//to provide the dataservice application wide
+import { DataService } from './services/data.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,10 @@ import { ProblemListComponent } from './components/problem-list/problem-list.com
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
