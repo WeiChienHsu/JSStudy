b58d4f703ef1246d4a2331c5a5cf89fdfc1cfa0c
Bruce on W541 U18 <zhufuzz@hotmail.com>
2018-08-01 09:23:24 -0400

    /home/b/WebstormProjects/JSStudy/q0w9qo/ch3/router3
    start to work on routers
    
    Signed-off-by: Bruce on W541 U18 <zhufuzz@hotmail.com>

---
+++ q0w9qo/ch3/router3/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductComponent} from "./product/product.component";
+
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'product', component:ProductComponent},
+  //   {path:'/', component:HomeComponent},
+  // {path:'/product', component:ProductComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
