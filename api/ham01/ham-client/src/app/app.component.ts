de6b33d4c1f707b74ab177998bf0e2a43b508762
Bruce on W541 U18 <zhufuzz@hotmail.com>
2018-08-10 10:06:52 -0400

    api/ham01/ham-client
    install/set bootstrap and jquery
    
    Signed-off-by: Bruce on W541 U18 <zhufuzz@hotmail.com>

--- api/ham01/ham-client/src/app/app.component.ts
+++ api/ham01/ham-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hamilton Input File Generation System';
+
+/*  test() {
+    $("xxxx").show();
+
+  }*/
 }
--- bittiger_cs503/week01/oj-client/src/app/app.component.ts
+++ bittiger_cs503/week01/oj-client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from "jquery";
+// import * as $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  /*test(){
+    $("xxxx").show();
+  }*/
 }
