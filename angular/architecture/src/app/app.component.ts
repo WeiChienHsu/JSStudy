507518738e05a6768140a5eae6f126d0dd49f060
Bruce on W541 U18 <zhufuzz@hotmail.com>
2018-09-13 15:23:23 -0400

    /angular add more official angular example
    Signed-off-by: Bruce on W541 U18 <zhufuzz@hotmail.com>

---
+++ angular/architecture/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-hero-list></app-hero-list>
+    <app-sales-tax></app-sales-tax>
+  `
+})
+export class AppComponent { }
